829b0c2a6297a6b66a6f687525d3ed6681a3988e
ArtemPonkratov <artemka.g@mail.ru>
2023-08-21 16:23:54 +0200

    test
    1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,80 @@
-import React, { Component } from "react";
-import "./App.css";
+import { useState, useEffect, useRef } from "react";
 
-class App extends Component {
-  // Инициализация начального состояния
-  state = {
-    count: 0,
-  };
+// usePrevious: кастомный хук для получения предыдущего значения состояния или пропса.
+function usePrevious(value) {
+  // Используем ref для хранения предыдущего значения
+  const ref = useRef();
 
-  // Метод для увеличения счётчика
-  handleIncrement = () => {
-    this.setState((prevState) => ({
-      count: prevState.count + 1,
-    }));
-  };
+  // После каждого рендера обновляем ref
+  useEffect(() => {
+    ref.current = value;
+  });
 
-  // Метод для уменьшения счётчика
-  handleDecrement = () => {
-    this.setState((prevState) => ({
-      count: prevState.count - 1,
-    }));
-  };
+  // Возвращаем предыдущее значение
+  return ref.current;
+}
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Счётчик: {this.state.count}</h1>
-        <button onClick={this.handleIncrement}>Увеличить</button>
-        <button onClick={this.handleDecrement}>Уменьшить</button>
-      </div>
-    );
-  }
+// useToggle: кастомный хук для переключения булева значения (true/false).
+function useToggle(initialValue = false) {
+  // Состояние для хранения текущего значения
+  const [value, setValue] = useState(initialValue);
+
+  // Переключаем значение
+  const toggle = () => setValue(!value);
+
+  return [value, toggle];
+}
+
+// useInterval: кастомный хук для выполнения действия через определенные интервалы времени.
+function useInterval(callback, delay) {
+  // ref для хранения переданного callback
+  const savedCallback = useRef();
+
+  // Сохраняем последний переданный callback в ref
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Устанавливаем интервал и выполняем callback
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id); // Очищаем интервал при размонтировании или при изменении delay
+    }
+  }, [delay]);
+}
+
+function App() {
+  // Состояние для хранения текущего значения счетчика
+  const [count, setCount] = useState(0);
+
+  // Используем кастомный хук useToggle для управления автоматическим увеличением
+  const [isCounting, toggleIsCounting] = useToggle(false);
+
+  // Получаем предыдущее значение счетчика
+  const prevCount = usePrevious(count);
+
+  // Используем useInterval для автоматического увеличения счетчика каждую секунду, когда isCounting === true
+  useInterval(() => {
+    if (isCounting) {
+      setCount(count + 1);
+    }
+  }, 1000);
+
+  return (
+    <div>
+      <h1>Текущее значение: {count}</h1>
+      <h2>Предыдущее значение: {prevCount}</h2>
+      <button onClick={() => setCount(count + 1)}>Увеличить на 1</button>
+      <button onClick={toggleIsCounting}>
+        {isCounting ? "Остановить" : "Начать"} авто-увеличение
+      </button>
+    </div>
+  );
 }
 
 export default App;
